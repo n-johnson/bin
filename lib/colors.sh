d2ba53674d7e2915ff918ad6941984d797140a55
Nathan Johnson <n-johnson@users.noreply.github.com>
2016-08-12 12:56:35 -0700

    Adds prod_mongo, loadPrivate, exitIfError, print_error
      - Adds concept of "private" values that are stored outside of git
        to store sensitive information.

---
+++ lib/colors.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export COLOR_RED='\033[0;31m'
+export COLOR_GREEN='\033[0;32m'
+export COLOR_YELLOW='\033[0;33m'
+export COLOR_MAGENTA='\033[0;95m'
+export COLOR_BLUE='\033[0;94m'
+export COLOR_NC='\033[0m'
